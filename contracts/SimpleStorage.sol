b40ee61e8aed8489a824f670ec42b534603441b0
Jayman100 <garbajamiu2000@gmail.com>
2024-11-03 19:48:47 +0100

    fix: make some amendments in simple storage

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -10,38 +10,46 @@ contract SimpleStorage {
 
     //Types: String, boolean, address, uint, int, bytes
     // The variable favoriteNumber get initialized to zero
-    uint256 public favoriteNumber ;
+    uint256 public myFavoriteNumber ;
 
     // How mapping works?
     mapping (string => uint256) public  nameToFavoriteNumber;
 
     //Struct is like a class in OOP?
-    struct People {
+    struct Person {
         uint256 favoriteNumber;
         string name;
     }
 
-    //Instance of People struct
-    People[ ] public  person;
+
+    //-> Using the custom type Person to create data for jay and you , a better way is to use
+    // Array (list of data ) , because what if we have more than 2 people to add ?
+
+    // Person public jay = Person({favoriteNumber: 5, name: "jay"});
+    // Person public you = Person({favoriteNumber: 10, name: "You"});
+ 
+
+    // -> An array of Person struct (default to an empty array , until we add data)
+    Person[] public  people;
 
 
     function store (uint256 _favoriteNumber) public virtual  {
-        favoriteNumber = _favoriteNumber;
+        myFavoriteNumber = _favoriteNumber;
     } 
 
 
     function retreive() public  view returns(uint256) {
-        return  favoriteNumber;
+        return  myFavoriteNumber;
     }
 
 
     //calldata, memory, storage
     function addPerson(string calldata _name, uint256 _favoriteNumber) public  {
-        //Another way to push to an array -> People memory newPerson = People({favoriteNumber: _favoriteNumber, name: _name});
+        //Another way to push to an array -> Person memory newPerson = Person({favoriteNumber: _favoriteNumber, name: _name});
         
-        // person.push(newPerson);
+        // Person.push(newPerson);
 
-        person.push(People(_favoriteNumber, _name));
+        people.push(Person(_favoriteNumber, _name));
         nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 
