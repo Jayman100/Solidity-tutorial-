c770fbdc6f76b16e0e86375e4a6ef1f2a97bb6df
Jayman100 <garbajamiu2000@gmail.com>
2024-11-07 15:57:06 +0100

    add more function to storage factory

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -1,16 +1,29 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.19;
+// This kind of import works like pasting the whole contract here(another contract) ,
+// But what if we have more than one contract in there, solidity will
+// have to do thhe computations for other contract too during transaction,
+// so we will have to pay more for the tx. So solve this we should always 
+// use named import to import the specific contract we need 
+// import "./SimpleStorage.sol";
 
-import "./SimpleStorage.sol";
+// NAMED IMPORT
+import {SimpleStorage} from "./SimpleStorage.sol";
 
+// Deploying a contract from another contract
 contract StorageFactory {
 
-    SimpleStorage[] public  simpleStorageArray;
+    SimpleStorage[] public  listOfSimpleStorageContracts;
 
     function createSimpleStorageContract() public  {
+
+        // Create a new SimpleStorage 
        SimpleStorage simpleStorage = new SimpleStorage();
-       simpleStorageArray.push(simpleStorage);
+
+        // Add the currently created simpleStorage in an array to keep track
+        // of the simpleStorage contracts we are creating  
+       listOfSimpleStorageContracts.push(simpleStorage);
     }
 
 
@@ -18,19 +31,38 @@ contract StorageFactory {
         //In order to interact with a contract , we need :
         //- Address
         //- ABI (Application binary interface)
-        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        SimpleStorage simpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
         simpleStorage.store(_simpleStorageNumber);
 
     }
 
     function sfGet(uint256 _simpleStorageIndex) public  view returns(uint256) {
-        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        SimpleStorage simpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        
         return  simpleStorage.retreive();
 
         //We can also do chaining too:
         
         // simpleStorageArray[_simpleStorageIndex].retrieve(); -> This work for the "store" function too
     }
+
+    function sfAddPerson(uint256 _simpleStorageIndex, string calldata _simpleStorageName, uint256 _simpleStorageFavNumber) public {
+        SimpleStorage simpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        simpleStorage.addPerson(_simpleStorageName, _simpleStorageFavNumber);
+    
+
+    }
+    
+
+    function sfGetPersonFavNum(uint256 _simpleStorageIndex, string calldata _simpleStorageName) public view returns(uint256) {
+            return    listOfSimpleStorageContracts[_simpleStorageIndex].nameToFavoriteNumber(_simpleStorageName);
+
+    }
+
+
+
+
+   
 
 
 }
